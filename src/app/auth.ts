9e1d482df6a2807994f2f61215ef0ede3efda568
ArthurFR <contato.arthurfr@gmail.com>
2023-12-21 00:22:27 -0300

    Add scope repo to github auth

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,3 +1,25 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-export const { handlers: { GET, POST }, auth, signIn } = NextAuth({providers: [ GitHub ]})
+export const { handlers: { GET, POST }, auth, signIn } = NextAuth({
+    providers: [
+        GitHub({
+            authorization: { params: { scope: "read:user user:email public_repo"}},
+        })
+    ],
+    callbacks: {
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+              token.accessToken = account.access_token
+            }
+            console.log('jwt', account)
+            return token
+        },
+        async session({ session, token, user }) {
+          // Send properties to the client, like an access_token from a provider.
+          console.log('sessions', session)
+          session.accessToken = token.accessToken
+          return session
+        }
+    }
+})
